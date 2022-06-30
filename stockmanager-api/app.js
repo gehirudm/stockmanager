12c4c916cf137ff5501c06e6886779c25d399bc6
Gehiru Damnidu <gehirudm@pm.me>
2022-06-30 11:59:36 +0530

    setting up project

---
+++ stockmanager-api/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBrlZWkwyUmNOuJiZMspKY8XobCIanSgyo",
+  authDomain: "stockmanager-6a916.firebaseapp.com",
+  projectId: "stockmanager-6a916",
+  storageBucket: "stockmanager-6a916.appspot.com",
+  messagingSenderId: "1039598534145",
+  appId: "1:1039598534145:web:7fca6911280828cbc80a00",
+  measurementId: "G-SGDX1YLEN6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
