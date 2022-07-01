ec9e316e3eeaacc79c23b30ff915c25c3ff41d1b
Gehiru Damnidu <gehirudm@pm.me>
2022-07-01 20:02:57 +0530

    basic react ui

--- stockmanager-web/src/App.js
+++ stockmanager-web/src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const NavbarControls = ({itemAdd}) => {
+  const [textFieldValue, textFieldValueSet] = useState('');
+
+  const handleChange = (event) => {
+    textFieldValueSet(event.target.value);
+  }
+
+  const handleClick = () => {
+    itemAdd(textFieldValue);
+  };
+
+  return <div>
+    <input type="text" onChange={handleChange} value={textFieldValue}></input>
+    <div className='Button Button-Blue' onClick={handleClick}>Add Item</div>
+  </div>
+}
+
+const Button = ({itemAdd}) => {
+  return <div className='Button Button-Blue' onClick={itemAdd}>Add Item</div>
+}
+
+const ItemCard = ({ itemName, itemAmount }) => {
+  const [count, setCount] = useState(itemAmount);
+
+  const handleClickUp = () => {
+    setCount(count + 1)
+  }
+
+  const handleClickDown = () => {
+    setCount(count - 1)
+  }
+
+  return <div className='Item-Card'>
+    <h2>{itemName}</h2>
+    <h3>Available : {count}</h3>
+    <div className='Item-Card-Controls'>
+      <span className='Item-Card-Controls-Increase' onClick={handleClickUp}>+</span>
+      <span className='Item-Card-Controls-Decrease' onClick={handleClickDown}>-</span>
+    </div>
+  </div>
+}
+
 function App() {
+
+  const [cards, setCards] = useState([]);
+  //Generate the cards
+  useEffect(() => {
+    setCards([
+      {
+        Name: "pumpkin",
+        Amount: 60
+      },
+      {
+        Name: "carrot",
+        Amount: 60
+      },
+      {
+        Name: "beetroot",
+        Amount: 60
+      },
+      {
+        Name: "apple",
+        Amount: 60
+      },
+    ])
+  }, [])
+
+  const handleItemAdd = (newItemName) => {
+    setCards([... cards, {
+      Name: newItemName,
+      Amount: 0
+    }])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Navbar'>
+        <NavbarControls itemAdd={handleItemAdd}></NavbarControls>
+      </div>
+      <div className='Cards-Container'>
+        {cards.map((card, index) => {
+          return <ItemCard itemName={card.Name} itemAmount={card.Amount} key={index}></ItemCard>
+        })}
+      </div>
     </div>
   );
 }
---
+++ stockmanager-web/src/components.js
@@ -0,0 +1,5 @@
+import React, { Component, useCallback, useState } from 'react';
+
+
+
+export default { Button }
